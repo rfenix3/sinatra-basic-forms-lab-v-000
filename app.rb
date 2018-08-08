fa1b74c6b4e8bf3256f04d03d5faf0cbb97bc98a
Richard Fenix <richard.fenix@gmail.com>
2018-08-08 12:10:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
+
 
 class App < Sinatra::Base
   get '/' do
     erb :index
   end
 
-  
+  post '/piglatinize' do
+    pl = Piglatinize.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    erb :results
+  end
 end
